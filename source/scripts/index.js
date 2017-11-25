fdfb598708675cf22eee8c192b30b8407ff9fa17
Kingson Chinedu Odogwu <kcodogwu@localhost.localdomain>
2017-11-25 23:14:18 +0000

    Top data display

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -7,40 +7,22 @@ var LIB = {
     if (result.length > 1)
       return [].slice.call(result, 0)
     ; else
-      return result
+      return result[0]
     ;
   },
-  getCORS: function getCORS(url, success) {
-    var xhr = new XMLHttpRequest();
+  getAjax: function getAjax(url, success) {
+    var xhr = window.XMLHttpRequest ? new XMLHttpRequest() : (new ActiveXObject('Msxml2.XMLHTTP') || new ActiveXObject('Microsoft.XMLHTTP'));
 
-    if (xhr.withCredentials === undefined) xhr = new XDomainRequest();
     xhr.open('GET', url);
 
-    xhr.onload = function xhr_onload(request) {
-      var res = request.currentTarget || request.target;
-    
-      if (res.responseText !== undefined)
-        success(res.responseText)
-      ; else
-        success(res.response)
-      ;
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState>3 && xhr.status==200) {
+        success(xhr);
+      }
     };
-
+    
     xhr.send();
     return xhr;
-  },
-  parseXML: function parseXML(str) {
-    var xmlDoc = str;
-
-    if (window.DOMParser) 
-      xmlDoc = (new DOMParser()).parseFromString(str, 'text/xml')
-    ; else {
-      xmlDoc = new ActiveXObject('Microsoft.XMLDOM');
-      xmlDoc.async = false;
-      xmlDoc.loadXMl(str);
-    }
-
-    return xmlDoc;
   },
   addEvent: function addEvent(el, type, handler) {
     if (el.attachEvent)
@@ -52,20 +34,55 @@ var LIB = {
 };
 
 window.onload = function onload() {
-  //var div = LIB._$('div');
-
-  LIB.getCORS(
-    'http://api.openweathermap.org/data/2.5/forecast?q=Athlone,ie&units=metric&mode=xml&type=like&appid=fa868a7f62f1ab1e638c07217f015307',
+  LIB.getAjax(
+    'http://api.openweathermap.org/data/2.5/weather?q=Athlone,ie&units=metric&mode=xml&type=like&appid=fa868a7f62f1ab1e638c07217f015307',
     function success(data) {
-      console.log(data);
+      var xmlData = data.responseXML;
+      var cityName = xmlData.getElementsByTagName('city')[0].getAttribute('name');
+      var temperature = xmlData.getElementsByTagName('temperature')[0];
+      var tempValue = temperature.getAttribute('value');
+      var tempMin = temperature.getAttribute('min');
+      var tempMax = temperature.getAttribute('max');
+      var clouds = xmlData.getElementsByTagName('clouds')[0].getAttribute('name');
+      var divTop = LIB._$('div.top');
+      var h3 = document.createElement('h3');
+      var h2 = document.createElement('h1');
+      var p = document.createElement('p');
+      var p1 = document.createElement('p');
+      var sup = document.createElement('sup');
+      
+      h3.appendChild(document.createTextNode(cityName));
+      p.appendChild(document.createTextNode(clouds));
+      h2.appendChild(document.createTextNode(tempValue));
+      sup.appendChild(document.createTextNode('o'));
+      h2.appendChild(sup);
+      h2.appendChild(document.createTextNode('C'));
+      p1.appendChild(document.createTextNode('Min: ' + tempMin + ' Max: ' + tempMax));
+      divTop.appendChild(h3);
+      divTop.appendChild(p);
+      divTop.appendChild(h2);
+      divTop.appendChild(p1);
+
+      // console.log(cityName);
+      // console.log(tempValue);
+      // console.log(tempMin);
+      // console.log(tempMax);
+      // console.log(clouds);
     }
   );
 
-  LIB.getCORS(
+  LIB.getAjax(
     'http://api.openweathermap.org/data/2.5/forecast?q=Athlone,ie&units=metric&type=like&appid=fa868a7f62f1ab1e638c07217f015307',
     function success(data) {
-      
-      console.log(JSON.parse(data));
+      console.log(JSON.parse(data.responseText));
     }
   );
+
+  // LIB.getAjax(
+  //   'http://api.openweathermap.org/data/2.5/forecast?q=Athlone,ie&units=metric&type=like&appid=fa868a7f62f1ab1e638c07217f015307',
+  //   function success(data) {
+      
+  //     console.log(JSON.parse(data));
+  //   }
+  // );
 };
